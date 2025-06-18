cc946f0c3ad66af0d3cfa146bea89486a0b9a341
j4yesh-911 <jayeshgayri12@gmail.com>
2025-06-18 23:24:34 +0530

    binary search using for

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -19,35 +19,81 @@
 //                                             BINARY SEARCH
 
 
- #include<iostream>
-using namespace std;
-int bs(int arr[],int n,int key){
-int s=0 ; int e=n;
-while(s<=e){
-    int mid =(s+e)/2;
-    if (arr[mid]==key){
-        return mid;
-    }
-    else if (arr[mid]>key){
-        e=mid-1;
-    }
-    else{
-        s=mid+1;
-    }
-}
-cout<<"no ele\n";
-return 0;
-}
-int main (){
-        int n;
-           cin>>n;
+//  #include<iostream>
+// using namespace std;
+// int bs(int arr[],int n,int key){
+// int s=0 ; int e=n;
+// while(s<=e){
+//     int mid =(s+e)/2;
+//     if (arr[mid]==key){
+//         return mid;
+//     }
+//     else if (arr[mid]>key){
+//         e=mid-1;
+//     }
+//     else{
+//         s=mid+1;
+//     }
+// }
+// cout<<"no ele\n";
+// return 0;
+// }
+// int main (){
+//         int n;
+//            cin>>n;
 
-           int arr[n];
-           for(int i = 0;i<n;i++){
-               cin>>arr[i];
-           }
-    int key;
-    cin>>key;
-    cout<<bs(arr,n,key)<<endl;
-    return 0;
+//            int arr[n];
+//            for(int i = 0;i<n;i++){
+//                cin>>arr[i];
+//            }
+//     int key;
+//     cin>>key;
+//     cout<<bs(arr,n,key)<<endl;
+//     return 0;
+// }
+
+//                                ANOTHER VERSION "BINARY SEARCH" 
+
+#include <iostream>
+using namespace std;
+int main(){
+  system("cls");
+int e,mid,s;
+int arr[8]={1,3,3,5,7,9,19,29};
+s=0;
+e=8;
+mid = (s+e)/2;
+int key;
+cout<<"enter the key to be found: ";
+cin>>key;
+for(int i=s;i<e;i++){
+if (arr[mid] == key){
+  cout<<"key found in the mid";
+  // return key;
+}
+}
+if(key>arr[mid]){
+  for(int i =mid+1; i<e;i++){
+    if (arr[i]==key){
+      cout<<"key found\n";
+        cout<<"key "<<arr[i]<<"found at this pos:"<<i;
+          return 0;
+    }
+  }
+}
+if(key<arr[mid]){
+  for(int i=s;i<mid;i++){
+    if (arr[i]==key){
+       cout<<"key found\n";
+       cout<<"key "<<arr[i]<<"found at this pos:"<<i;
+           return 0;
+    }
+  }
+
+}
+
+  cout<<"key not found";
+
+
+
 }
