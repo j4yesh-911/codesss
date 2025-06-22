b35c1993e9a73c8c78d98fcac9129821815e9c0e
j4yesh-911 <jayeshgayri12@gmail.com>
2025-06-22 22:49:24 +0530

    c programming

--- pactice.c
+++ pactice.c
@@ -67,12 +67,151 @@
 
 
 
+// #include <stdio.h>
+// void main (){
+//     int c;
+//     c =getchar ();
+//     while (c!= EOF){
+//         putchar(c);
+//        c= getchar ();
+//     }
+// }
+
+
+// #include <stdio.h>
+
+// int main(void) {
+//     int c;
+//     while ((c = getchar()) != EOF) { 
+//here we used parentheses because precdence of " != i " higher then " = ";
+//         printf("%d ", c != EOF);
+//         putchar(c);
+//     }
+//     printf("\n%d\n", c != EOF);
+//     return 0;
+
+// }
+
+// #include <stdio.h>
+// int main(){
+// printf("%d",EOF);
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+//     long nc;  //long = lpng int;
+//     nc=0;
+//     while(getchar()!=EOF){
+//         ++nc;
+//         printf("%6ld",nc);
+//     }
+// }
+
+
+// #include <stdio.h>
+// int main(){
+
+//     //line counting program
+
+//     long nc;  
+//     int c;
+//     nc=0;
+//     while((c=getchar())!=EOF){
+//         if(c==' ')
+//         ++nc;
+//         printf("%6ld",nc);
+//     }
+// }
+
+
+// #include <stdio.h>
+// #define out 0
+// #define in 1
+// int main(){
+//     int c, state;
+//    long int nw,nl,nc;
+//    state =out;
+//  nw,nl,nc=0;
+//  while((c=getchar())!=EOF){
+//     ++nc;
+//     if(c=='\n'){
+//         ++nl;
+//     }
+//     if(c==' '||c=='\n'||c=='\t')
+//         state =out;
+//         else if (state==out){
+//         state = in ;
+//         ++nw;
+//     }
+//  }
+//  printf("%ld,%ld,%ld\n",nl,nw,nc);
+// }
+
+
+
+
+// #include <stdio.h>
+
+// /* count digits, white space, others */
+// main() {
+//     int c, i, nwhite, nother;
+//     int ndigit[10];
+
+//     nwhite = nother = 0;
+//     for (i = 0; i < 10; ++i)
+//         ndigit[i] = 0;
+
+//     while ((c = getchar()) != EOF)
+//         if (c >= '0' && c <= '9')
+//             ++ndigit[c - '0'];
+//         else if (c == ' ' || c == '\n' || c == '\t')
+//             ++nwhite;
+//         else
+//             ++nother;
+
+//     printf("digits =");
+//     for (i = 0; i < 10; ++i)
+//         printf(" %d", ndigit[i]);
+//     printf(", white space = %d, other = %d\n",
+//            nwhite, nother);
+           
+// }
+
+
+
+// #include <stdio.h>
+//    int power(int i, int n);
+// int main(){
+//     for(int i =0; i<10;i++){
+//         printf("%d,%d,%d",i,power(2,i),power(-3,i));
+//         // return 0; 
+//     }
+// }
+// int power(int base, int n) {
+//     int i, p;
+//     p = 1;
+//     for (i = 1; i <= n; ++i)
+//         p = p * base;
+//     return p;
+// }
+
+
 #include <stdio.h>
-main (){
-    int c;
-    c =getchar ();
-    while (c!= EOF){
-        putchar(c);
-       c= getchar ();
-    }
+int power(int  , int  );
+int main (){
+    for(int i =0;i<10;i++){
+printf("%d,%d,%d",i,power(2,i),power(-3,i));
+return 0;
 }
+}
+int power(int base , int n){
+    int p;
+    p=1;
+    for(int i=1;i<=n;++i)
+    p=p*base;
+    return p;
+
+}
+
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,15 @@
-
-#include <stdio.h>
-main (){
-    int c;
-    c =getchar ();
-    while (c!= EOF){
-        putchar(c);
-       c= getchar ();
-    }
-}
+/ #include <stdio.h>
+//    int power(int i, int n);
+// int main(){
+//     for(int i =0; i<10;i++){
+//         printf("%d,%d,%d",i,power(2,i),power(-3,i));
+//         // return 0; 
+//     }
+// }
+// int power(int base, int n) {
+//     int i, p;
+//     p = 1;
+//     for (i = 1; i <= n; ++i)
+//         p = p * base;
+//     return p;
+// }
