bfd2a00aa756eb37f23cbd26acb096cca46d9988
j4yesh-911 <jayeshgayri12@gmail.com>
2025-06-29 18:57:03 +0530

    c programming

--- pactice.c
+++ pactice.c
@@ -126,28 +126,28 @@
 // }
 
 
-#include <stdio.h>
-#define out 0
-#define in 1
-int main(){
-    int c, state;
-   long int nw,nl,nc;
-   state =out;
- nw=nl=nc=0;
- while((c=getchar())!=EOF){
-    ++nc;
-    if(c=='\n'){
-        ++nl;
-    }
-    if(c==' '||c=='\n'||c=='\t')
-        state =out;
-        else if (state==out){
-        state = in ;
-        ++nw;
-    }
- }
- printf("%ld,%ld,%ld\n",nl,nw,nc);
-}
+// #include <stdio.h>
+// #define out 0
+// #define in 1
+// int main(){
+//     int c, state;
+//    long int nw,nl,nc;
+//    state =out;
+//  nw=nl=nc=0;
+//  while((c=getchar())!=EOF){
+//     ++nc;
+//     if(c=='\n'){
+//         ++nl;
+//     }
+//     if(c==' '||c=='\n'||c=='\t')
+//         state =out;
+//         else if (state==out){
+//         state = in ;
+//         ++nw;
+//     }
+//  }
+//  printf("%ld,%ld,%ld\n",nl,nw,nc);
+// }
 
 
 
@@ -198,20 +198,93 @@ int main(){
 // }
 
 
+// #include <stdio.h>
+// int power(int base , int n){
+//     int p;
+//     p=1;
+//     for(int i=1;i<=n;++i)
+//     p=p*base;
+//     return p;
+
+// }
+// int main (){
+//     for(int i =0;i<10;i++){
+// printf("%d,%d,%d",i,power(2,i),power(-3,i));
+// }
+// return 0;
+// }
+
+
+// #include <stdio.h>
+// int strlens(char a[]){
+//     int i;
+//     i =0; 
+//     while (a[i]!='\0'){
+//         ++i;
+//     }
+//     return i;
+// }
+// int main(){
+//     char c[]="hello_jayeshhjyhyjhtgfarhagaianahymjulkiopqazxswedcvfrtgbnhyujmki";
+//    int count = strlens(c);
+//     printf("%c",count);
+//
+
+
+//                                           ASCII
+
+// #include<stdio.h>
+// int main(){
+//     system ("cls");
+//     // for(int n= 33; n<255;n++){
+//     // printf("%c",n);
+// //}
+//     int a=7;
+//  printf("%c",a);
+//     
+
+
+
+
+//                                            BEEP
+
+// #include <windows.h>
+// int main() {
+//     Beep(250, 3000); 
+//     return 0;
+// }
+
+
+
+//                                              DELAY
+
+
+// #include <stdio.h>
+// #include <windows.h>  // For Sleep()
+
+// int main() {
+//     printf("Start\n");
+//     Sleep(1000);  // Delay for 1000 milliseconds = 1 second
+//     printf("1 second later...\n");
+
+//     Sleep(2000);  // Delay for 2 seconds
+//     printf("3 seconds passed in total!\n");
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
-int power(int base , int n){
-    int p;
-    p=1;
-    for(int i=1;i<=n;++i)
-    p=p*base;
-    return p;
-
-}
 int main (){
-    for(int i =0;i<10;i++){
-printf("%d,%d,%d",i,power(2,i),power(-3,i));
-}
-return 0;
-}
+    enum day{
+        sunday ,monday,tuesday,wednesday,thursay,friday,saturday
+    };
 
+  
+        enum day today= monday;
+    printf("%d",today);
 
+char esc ='\\';
+float e = 1.0e-5;
+printf("%f",e);
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-int power(int base , int n){
-    int p;
-    p=123456789L;
-    for(int i=1;i<=n;++i)
-    p=p*base;
-    return p;
-
+int main(){
+    int strlen(char s[]){
+        int i;
+        i=0;
+        while (s[i]!= '0')
+        ++i;
+        return i;
+    
 }
-int main (){
-    for(int i =0;i<10;i++){
-printf("%d,%d,%d",i,power(2,i),power(-3,i));
-}
-return 0;
 }
