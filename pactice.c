daf224ab258bf9032d95caad820b6a9b796d2ecf
j4yesh-911 <jayeshgayri12@gmail.com>
2025-07-15 23:10:07 +0530

    c programming

--- pactice.c
+++ pactice.c
@@ -317,10 +317,28 @@
 
 
 
+// #include <stdio.h>
+// int main (){
+//     char c [3]={'a','b','c'};
+//     for(int i=0; i<3;i++){
+//     printf("%c",c[i]);
+//     }
+// }
+
+
+
 #include <stdio.h>
-int main (){
-    char c [3]={'a','b','c'};
-    for(int i=0; i<3;i++){
-    printf("%c",c[i]);
-    }
+
+int main() {
+    char ch;
+    printf("Enter a letter: ");
+    scanf("%c", &ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+        printf("%c is a vowel.\n", ch);
+    else
+        printf("%c is a consonant.\n", ch);
+
+    return 0;
 }
